df867d98336f47d11f059a1796c03f54d0ad956e
Joseph Joyce <joej326@gmail.com>
2018-12-01 17:44:47 -0500

    beta

--- main.js
+++ main.js
@@ -19,22 +19,24 @@ document.body.append(player);
    
 
 // INTERVAL ////////////
-let powerUpIntervalStart = 5000;
+let powerUpIntervalStart = 4000;
 
-function startInterval() {
+function startPowerUpInterval(go) {
     const powerUpInterval = setInterval(() => {
-        if (powerUpIntervalStart > 4000 && powerUpIntervalStart <= 5000) {
-            powerUpIntervalStart -= 200;
-        } else if (powerUpIntervalStart > 3000 && powerUpIntervalStart <= 4000) {
-            powerUpIntervalStart -= 150;
-        } else if (powerUpIntervalStart > 500 && powerUpIntervalStart <= 3000) {
+        if (powerUpIntervalStart > 2000 && powerUpIntervalStart <= 4000) {
             powerUpIntervalStart -= 100;
-        } else if (powerUpIntervalStart < 500) {
+        } else if (powerUpIntervalStart <= 2000) {
             powerUpIntervalStart = powerUpIntervalStart;
         }
-        generatePowerUp();
-        clearInterval(powerUpInterval);
-        startInterval();
+        if (go) {
+            generatePowerUp();
+            startPowerUpInterval(true);
+            clearInterval(powerUpInterval);
+
+        } else {
+            startPowerUpInterval(false);
+            clearInterval(powerUpInterval);
+        }
     
     }, powerUpIntervalStart);
 }
@@ -43,54 +45,66 @@ function startInterval() {
 
 
 generatePowerUp();
-startInterval();
+startPowerUpInterval(true);
+movePlayer();
 
 
 let playerTop = 0;
 let playerLeft = 0;
-document.addEventListener('click', (e) => {
-    console.log(e);
 
-    if (e.path[0].classList[1] === 'up-arrow') {
-        player.style.top = (playerTop -= 5) + 'vw';
-    } else if (e.path[0].classList[1] === 'down-arrow') {
-        player.style.top = (playerTop += 5) + 'vw';
-    } else if (e.path[0].classList[1] === 'left-arrow') {
-        player.style.left = (playerLeft -= 5) + 'vw';
-        player.style.transform = 'rotateY(0deg)';
-    } else if (e.path[0].classList[1] === 'right-arrow') {
-        player.style.left = (playerLeft += 5) + 'vw';
-        player.style.transform = 'rotateY(180deg)';
-    }
-});
+function movePlayer() {
+    document.querySelector('.mobile-controller').addEventListener('click', (e) => {
+        console.log(e);
+    
+        if (e.path[0].classList[1] === 'up-arrow') {
+            player.style.top = (playerTop -= 5) + 'vw';
+        } else if (e.path[0].classList[1] === 'down-arrow') {
+            player.style.top = (playerTop += 5) + 'vw';
+        } else if (e.path[0].classList[1] === 'left-arrow') {
+            player.style.left = (playerLeft -= 5) + 'vw';
+            player.style.transform = 'rotateY(0deg)';
+        } else if (e.path[0].classList[1] === 'right-arrow') {
+            player.style.left = (playerLeft += 5) + 'vw';
+            player.style.transform = 'rotateY(180deg)';
+        }
 
-document.addEventListener('keydown', (e) => {
-    if (e.code === 'ArrowUp') {
-        player.style.top = (playerTop -= 5) + 'vw';
-    } else if (e.code === 'ArrowDown') {
-        player.style.top = (playerTop += 5) + 'vw';
-    } else if (e.code === 'ArrowLeft') {
-        player.style.left = (playerLeft -= 5) + 'vw';
-        player.style.transform = 'rotateY(0deg)';
-    } else if (e.code === 'ArrowRight') {
-        player.style.left = (playerLeft += 5) + 'vw';
-        player.style.transform = 'rotateY(180deg)';
-    } else if (e.code === 'ArrowRight' && e.code === 'ArrowUp') {
-        player.style.left = (playerLeft += 5) + 'vw';
-        player.style.top = (playerTop -= 5) + 'vw';
-        player.style.transform = 'rotateY(180deg)';
-    } 
+        setInterval(() => {
+            detectEntityCollision();
+        }, 200);
+        scoreKeeperElem.innerHTML = 'Score: ' + score;
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'ArrowUp') {
+            player.style.top = (playerTop -= 5) + 'vw';
+        } else if (e.code === 'ArrowDown') {
+            player.style.top = (playerTop += 5) + 'vw';
+        } else if (e.code === 'ArrowLeft') {
+            player.style.left = (playerLeft -= 5) + 'vw';
+            player.style.transform = 'rotateY(0deg)';
+        } else if (e.code === 'ArrowRight') {
+            player.style.left = (playerLeft += 5) + 'vw';
+            player.style.transform = 'rotateY(180deg)';
+        } else if (e.code === 'ArrowRight' && e.code === 'ArrowUp') {
+            player.style.left = (playerLeft += 5) + 'vw';
+            player.style.top = (playerTop -= 5) + 'vw';
+            player.style.transform = 'rotateY(180deg)';
+        } 
+    
+        setInterval(() => {
+            detectEntityCollision();
+        }, 200);
+        scoreKeeperElem.innerHTML = 'Score: ' + score;
+    
+    });
+}
 
-    detectPowerUpCollision();
-    scoreKeeperElem.innerHTML = 'Score: ' + score;
-
-});
 
 
 function generatePowerUp() {
     let powerUpId = 1;
-    const leftPos = (Math.floor(Math.random() * Math.floor(100)).toString() + 'vw');
-    const topPos = (Math.floor(Math.random() * Math.floor(99)).toString() + 'vh');
+    const leftPos = (Math.floor(Math.random() * Math.floor(90)).toString() + 'vw');
+    const topPos = (Math.floor(Math.random() * Math.floor(90)).toString() + 'vh');
     
 
 
@@ -111,14 +125,14 @@ function generateHazard(direction) {
     let leftStart;
     let leftFinish;
     if (direction === 'leftToRight') {
-        leftStart = '-5vw';
+        leftStart = '-15vw';
         topStart = (Math.floor(Math.random() * Math.floor(99)).toString() + 'vh');
     } else if (direction === 'rightToLeft') {
         leftStart = '105vw';
         topStart = (Math.floor(Math.random() * Math.floor(99)).toString() + 'vh');
     } else if (direction === 'topToBottom') {
         leftStart = (Math.floor(Math.random() * Math.floor(99)).toString() + 'vw');
-        topStart = '-5vh';
+        topStart = '-15vh';
     } else if (direction === 'bottomToTop') {
         leftStart = (Math.floor(Math.random() * Math.floor(99)).toString() + 'vw');
         topStart = '105vh';
@@ -134,31 +148,31 @@ function generateHazard(direction) {
 
     let movingPos;
     if (direction === 'leftToRight' || direction === 'topToBottom') {
-        movingPos = 0;
+        movingPos = -15;
     } else {
-        movingPos = 105;
+        movingPos = 115;
     }
     const moveHazardInterval = setInterval(() => {
         if (direction === 'leftToRight') {
-            if (movingPos < 105) {
+            if (movingPos < 115) {
                 hazard.style.left = `${movingPos += 0.5}vw`;
             } else {
                 clearInterval(moveHazardInterval);
             }
         } else if (direction === 'rightToLeft') {
-            if (movingPos > -5) {
+            if (movingPos > -25) {
                 hazard.style.left = `${movingPos -= 0.5}vw`;
             } else {
                 clearInterval(moveHazardInterval);
             }
         } else if (direction === 'topToBottom') {
-            if (movingPos < 105) {
+            if (movingPos < 115) {
                 hazard.style.top = `${movingPos += 0.5}vh`;
             } else {
                 clearInterval(moveHazardInterval);
             }
         } else if (direction === 'bottomToTop') {
-            if (movingPos > -5) {
+            if (movingPos > -25) {
                 hazard.style.top = `${movingPos -= 0.5}vh`;
             } else {
                 clearInterval(moveHazardInterval);
@@ -167,32 +181,113 @@ function generateHazard(direction) {
     }, 50);
 }
 
+let hazardIntervalStart = 2500;
+
+function startHazardInterval(go) {
+    const hazardInterval = setInterval(() => {
+        if (hazardIntervalStart > 1000 && hazardIntervalStart <= 2500) {
+            hazardIntervalStart -= 100;
+        } else {
+            hazardIntervalStart = hazardIntervalStart
+        }
+        const hazardDirectionArray = ['leftToRight', 'rightToLeft', 'topToBottom', 'bottomToTop'];
+        const randomDirection = hazardDirectionArray[(Math.floor(Math.random() * Math.floor(4)))];
+        console.log('before');
+        // generateHazard(randomDirection);
+        if (go) {
+            generateHazard(randomDirection);
+            startHazardInterval(true);
+            clearInterval(hazardInterval);
+
+        } else {
+            startHazardInterval(false);
+            clearInterval(hazardInterval);
+        }
+    
+    }, hazardIntervalStart);
+}
+
+startHazardInterval(true);
+// generateHazard('leftToRight');
+// generateHazard('rightToLeft');
+// generateHazard('topToBottom');
+// generateHazard('bottomToTop');
 
 
-generateHazard('leftToRight');
-generateHazard('rightToLeft');
-generateHazard('topToBottom');
-generateHazard('bottomToTop');
 
-
-
-function detectPowerUpCollision() {
-
+function detectEntityCollision() {
 
     const playerX = player.getBoundingClientRect().x;
     const playerY = player.getBoundingClientRect().y;
 
     const powerUpArray = [...document.querySelectorAll('.power-up')];
+    const hazardArray = [...document.querySelectorAll('.hazard')];
 
-    for (let i = 0; i < powerUpArray.length; i++) {
-        const powerUpX = powerUpArray[i].getBoundingClientRect().x;
-        const powerUpY = powerUpArray[i].getBoundingClientRect().y;
 
-        if ( (Math.abs((playerX - powerUpX) + 50) < 50) && (Math.abs((playerY - powerUpY) + 50) < 50) ) {
-            powerUpArray[i].remove();
-            score += 100;
+    // set collision radius for mobile
+    if ('ontouchstart' in document.documentElement) {
+        for (let i = 0; i < powerUpArray.length; i++) {
+            const powerUpX = powerUpArray[i].getBoundingClientRect().x;
+            const powerUpY = powerUpArray[i].getBoundingClientRect().y;
+    
+    
+            if ( (Math.abs((playerX - powerUpX) + 10) < 20) && (Math.abs((playerY - powerUpY) + 10) < 20) ) {
+                powerUpArray[i].remove();
+                score += 100;
+            }
+        }
+    
+        for (let i = 0; i < hazardArray.length; i++) {
+            const hazardX = hazardArray[i].getBoundingClientRect().x;
+            const hazardY = hazardArray[i].getBoundingClientRect().y;
+    
+            if ( (Math.abs((playerX - hazardX) + 10) < 10) && (Math.abs((playerY - hazardY) + 10) < 10) ) {
+                player.remove();
+                hazardArray.map((hazard) => hazard.remove());
+                powerUpArray.map((power) => power.remove());
+
+                startHazardInterval(false);
+                startPowerUpInterval(false);
+    
+                document.querySelector('.game-over').style.display = 'unset';
+                document.querySelector('.play-again').addEventListener('click', () => {
+                    window.location.reload(false);
+                });
+            }
+        }
+    } else if ( !('ontouchstart' in document.documentElement) ) {
+        for (let i = 0; i < powerUpArray.length; i++) {
+            const powerUpX = powerUpArray[i].getBoundingClientRect().x;
+            const powerUpY = powerUpArray[i].getBoundingClientRect().y;
+    
+    
+            if ( (Math.abs((playerX - powerUpX) + 50) < 60) && (Math.abs((playerY - powerUpY) + 50) < 60) ) {
+                powerUpArray[i].remove();
+                score += 100;
+            }
+        }
+    
+        for (let i = 0; i < hazardArray.length; i++) {
+            const hazardX = hazardArray[i].getBoundingClientRect().x;
+            const hazardY = hazardArray[i].getBoundingClientRect().y;
+    
+            if ( (Math.abs((playerX - hazardX) + 50) < 60) && (Math.abs((playerY - hazardY) + 50) < 60) ) {
+                player.remove();
+                hazardArray.map((hazard) => hazard.remove());
+                powerUpArray.map((power) => power.remove());
+
+                startHazardInterval(false);
+                startPowerUpInterval(false);
+    
+                document.querySelector('.game-over').style.display = 'unset';
+                document.querySelector('.play-again').addEventListener('click', () => {
+                    window.location.reload(false);
+                });
+            }
         }
     }
+
+    
 
 }
 
